aae851450b7210b6d798ddd18328709f6a1cf78c
FaysArukattil <faysarukattil@gmail.com>
2025-07-25 11:29:39 +0530

    Constructor in Super class

--- bin/inheritance_sample.dart
+++ bin/inheritance_sample.dart
@@ -1,27 +1,32 @@
 void main()
 {
-Student s1 = Student();
+Student s1 = Student("Fays", 19, "malappuram");
 s1.name = "Fays";
 print(s1.name);
+print(s1.age);
+print(s1.place);
 
 }
 class Person{
-  String name="";
-  int age=0;
-  String place="";
+  String name;
+  int age;
+  String place;
+  Person(this.name,this.age,this.place);
+  
 }
 class Student extends Person
 {
   int mark=0;
   String studentid="";
   String course="";
+  Student(super.name,super.age,super.place);
   
 }
-class Employee extends Person
-{
-  int salary=0;
-  String employeeid="";
-}
+// class Employee extends Person
+// {
+//   int salary=0;
+//   String employeeid="";
+// }
 
 
 
