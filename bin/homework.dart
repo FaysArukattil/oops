3aa894caace078ccf8087f75702ecbf53725e8c0
FaysArukattil <faysarukattil@gmail.com>
2025-07-28 14:46:42 +0530

    Completed

--- bin/homework.dart
+++ bin/homework.dart
@@ -1,34 +1,70 @@
-void main()
-{
+// void main()
+// {
 
-Car c1 =Car(brand: "Toyota", year: 2016, seats: 7);
-c1.displayInfo();
-print("");
-c1.displayCarInfo();
+// Car c1 =Car(brand: "Toyota", year: 2016, seats: 7);
+// c1.displayInfo();
+// print("");
+// c1.displayCarInfo();
 
+// }
+// class Vehicle
+// {
+//   String brand;
+//   int year;
+//   Vehicle({required this.brand,required this.year});
 
-}
-class Vehicle
-{
-  String brand;
-  int year;
-  Vehicle({required this.brand,required this.year});
+//   displayInfo()
+//   {
+//     print("Brand: $brand , Year: $year");
+//   }
 
-  displayInfo()
-  {
-    print("Brand: $brand , Year: $year");
+// }
+
+// class Car extends Vehicle
+// {
+//   int seats;
+//   Car({required super.brand,required super.year,required this.seats});
+
+//     displayCarInfo()
+//   {
+//     displayInfo();
+//     print("No Of Seats: $seats");
+//   }
+// }
+
+class Shape {
+  void area() {
+    print("Calaculating Area of Shape....");
   }
 
+  Shape();
 }
 
-class Car extends Vehicle
-{
-  int seats;
-  Car({required super.brand,required super.year,required this.seats});
+class Rectangle extends Shape {
+  int length, breadth;
+  Rectangle({required this.length, required this.breadth});
 
-    displayCarInfo()
-  {
-    displayInfo();
-    print("No Of Seats: $seats");
+  @override
+  void area() {
+    print("Area of Rectangle is ${length * breadth}");
   }
+}
+
+class Circle extends Shape {
+  int radius;
+  @override
+  void area() {
+    print("Area of Circle is ${3.14 * radius * radius}");
+  }
+
+  Circle({required this.radius});
+}
+
+void main() {
+  Shape s1 = Shape();
+  Rectangle r1 = Rectangle(breadth: 5, length: 6);
+  Circle c1 = Circle(radius: 10);
+  s1.area();
+  r1.area();
+  c1.area();
 }
