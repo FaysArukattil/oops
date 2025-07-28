c9bba76c5f9925ab7255b2e556d55cb82d1fe57d
FaysArukattil <faysarukattil@gmail.com>
2025-07-28 11:40:17 +0530

    polymorphism

--- bin/polymorphism.dart
+++ bin/polymorphism.dart
@@ -1,4 +1,28 @@
+//polymorphism
+
+//method Overloading  X (Not in Dart) : Writing the Same name method in a Single Class by Simply Changing the Parameters.
+
+//method Overriding : Writing the Same name method on an inherited class using @override
+
+
 void main()
 {
-  
+Student s1=Student();
+s1.printDetails();
+}
+class Person
+{
+  void printDetails()
+  {
+    print("Iam a Person");
+  }
+
+
+}
+class Student extends Person
+{
+  @override
+  void printDetails() {
+   print("Iam a Student");
+  }
 }
